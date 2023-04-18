057685d31d5d2383c26e6d11e3033ed571358857
Ignacio-CI <ignacio.cerda.i@gmail.com>
2023-04-18 09:41:47 +0200

    tailwind update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+const { default: plugin } = require('tailwindcss');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./views/*.{html,ejs}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    
+  ],
 }
 
